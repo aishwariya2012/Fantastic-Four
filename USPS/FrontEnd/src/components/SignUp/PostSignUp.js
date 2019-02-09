8a48623bfb6d54526eb2ff297bfa3cbdc88aa208
Anoop Martin <33829132+SuperStriker@users.noreply.github.com>
2019-02-09 13:56:44 -0800

    viewgroups

--- USPS/FrontEnd/src/components/SignUp/PostSignUp.js
+++ USPS/FrontEnd/src/components/SignUp/PostSignUp.js
@@ -30,14 +30,23 @@ import './App.css';
     render(){
        
         return(
-
-           
-            <div>
-                
-            </div>    
-                   
-              
-                      
+<div>
+           <div class="text" style={{"top-margin":"200px", "color":"black","background-color":"rgb(20, 119, 141)","font-size": "14px","padding": "4px","max-width": "250px","top":"20","text-align": "center","border": "2px solid"}} >
+            <p><strong>Zipcode: 95113</strong></p>
+            <p><strong>No of letters:50</strong></p>
+<button style={{"border": "2px solid"}}>USPS driver</button>; 
+&nbsp;
+<button style={{"border": "2px solid"}}>Fleet Driver</button>             
+</div>
+ <div class="text" style={{"top-margin":"200px", "color":"black","background-color":"rgb(20, 119, 141)","font-size": "14px","padding": "4px","max-width": "250px","top":"20","text-align": "center","border": "2px solid"}} >
+ <p><strong>Zipcode: 95113</strong></p>
+ <p><strong>No of letters:50</strong></p>
+<button style={{"border": "2px solid"}}>USPS driver</button>; 
+&nbsp;
+<button style={{"border": "2px solid"}}>Fleet Driver</button>             
+</div>    
+       
+</div>
         )
     }
 }
